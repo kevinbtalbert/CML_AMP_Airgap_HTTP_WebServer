ef784ea9a363adb0b37c2b110e976493c29d780b
Kevin Talbert <ktalbert@cloudera.com>
2024-03-01 12:54:33 -0500

    finalized repo

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Renders the homepage with links to the YAML files.
-    return render_template('index.html'
+    return render_template('index.html')
 
 @app.route('/community-amp-catalog.yaml')
 def community():
