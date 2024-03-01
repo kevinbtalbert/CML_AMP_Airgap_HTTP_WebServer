ec2c7e86a691f6da2d74ab418615875b24a11528
Kevin Talbert <ktalbert@cloudera.com>
2024-03-01 13:22:20 -0500

    updated install script for offline use

--- install-dependencies.py
+++ install-dependencies.py
@@ -1,1 +1,7 @@
-!pip install --no-index --find-links=/pip-requirements -r requirements.txt
+!pip install /pip-requirements/click-8.1.7-py3-none-any.whl
+!pip install /pip-requirements/Flask-2.1.2-py3-none-any.whl
+!pip install /pip-requirements/itsdangerous-2.1.2-py3-none-any.whl
+!pip install /pip-requirements/Jinja2-3.1.3-py3-none-any.whl
+!pip install /pip-requirements/MarkupSafe-2.1.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+!pip install /pip-requirements/PyYAML-6.0-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl
+!pip install /pip-requirements/Werkzeug-2.2.2-py3-none-any.whl
