6aad7e5ea6e36ef4eac1812d94dcc5fc522cb5e8
Kevin Talbert <ktalbert@cloudera.com>
2024-03-01 13:25:28 -0500

    updated install dependencies

--- install-dependencies.py
+++ install-dependencies.py
@@ -1,7 +1,7 @@
-!pip install /pip-requirements/click-8.1.7-py3-none-any.whl
-!pip install /pip-requirements/Flask-2.1.2-py3-none-any.whl
-!pip install /pip-requirements/itsdangerous-2.1.2-py3-none-any.whl
-!pip install /pip-requirements/Jinja2-3.1.3-py3-none-any.whl
-!pip install /pip-requirements/MarkupSafe-2.1.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-!pip install /pip-requirements/PyYAML-6.0-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl
-!pip install /pip-requirements/Werkzeug-2.2.2-py3-none-any.whl
+!pip install /home/cdsw/pip-requirements/click-8.1.7-py3-none-any.whl
+!pip install /home/cdsw/pip-requirements/Flask-2.1.2-py3-none-any.whl
+!pip install /home/cdsw/pip-requirements/itsdangerous-2.1.2-py3-none-any.whl
+!pip install /home/cdsw/pip-requirements/Jinja2-3.1.3-py3-none-any.whl
+!pip install /home/cdsw/pip-requirements/MarkupSafe-2.1.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+!pip install /home/cdsw/pip-requirements/PyYAML-6.0-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl
+!pip install /home/cdsw/pip-requirements/Werkzeug-2.2.2-py3-none-any.whl
